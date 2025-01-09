375a208ce8ed6589c2fcd1e12904a2adfef5ca77
nickmills-25n <nicholas.mills.c@gmail.com>
2025-01-09 17:59:00 -0500

    added Band Name Generator

---
+++ Band Name Generator.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator!') # Welcome statement
+city_name = input('What is the name of the city you grew up in?\n') # Prompts user for the city the grew up in
+animal = input('What is your favorite animal? \n') # prompts user for their favorite animal
+print('You should name your band The ' + city_name + ' ' + animal + 's') # Combines city name with animal and adds an 's' to the end
